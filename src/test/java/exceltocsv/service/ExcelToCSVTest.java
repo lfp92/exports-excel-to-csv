ecb525330deb0a19ad173ca58821e5120991d7b9
Leonardo F Petrauskas <asd>
2021-10-27 18:46:50 -0300

    Adicao de teste

--- src/test/java/exceltocsv/service/ExcelToCSVTest.java
+++ src/test/java/exceltocsv/service/ExcelToCSVTest.java
@@ -1,5 +1,25 @@
 package exceltocsv.service;
 
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
 class ExcelToCSVTest {
+
+    private ExcelToCSV excelToCSV = new ExcelToCSV();
+
+    @Test
+    @SneakyThrows
+    void deveExtrairPlanilhas() {
+        excelToCSV.exportCSV("C:\\Users\\Leonardo\\Downloads\\ficha-teste.xlsx", "C:\\Users\\Leonardo\\Downloads\\teste");
+
+        Assertions.assertDoesNotThrow(() -> {
+            File fileA = new File("C:\\Users\\Leonardo\\Downloads\\teste_Ficha A.csv");
+            File fileB = new File("C:\\Users\\Leonardo\\Downloads\\teste_Ficha B.csv");
+        });
+
+    }
 
 }
